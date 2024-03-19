9218b71a9bd06ce926a72fe233fbbae0e38fa0e6
karthikowo <20pw16@psgtech.ac.in>
2024-03-19 20:04:42 +0530

    add-shadcn-table

--- src/app/editor/page.tsx
+++ src/app/editor/page.tsx
@@ -1,7 +1,15 @@
-// `app/page.tsx` is the UI for the `/` URL
-'use client'
-import Attachments from './components/Attachments';
+// pages/editor/page.tsx
 
-export default function Page() {
-    return <Attachments/>
-}
+import React from "react";
+import { DataTable } from "./components/Table";
+const EditorPage: React.FC = () => {
+  return (
+    <div className="">
+      <h1>Editor Page</h1>
+      <DataTable />
+      {/* Add your editor-related content here */}
+    </div>
+  );
+};
+
+export default EditorPage;
